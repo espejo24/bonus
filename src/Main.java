eee917a3ac2d7fd0d7e2f944ab6088a64d618800
Maria Markina <rbsk@bk.ru>
2022-01-10 23:24:02 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         int count = 100; // начальная сумма счета
-        int deposit_amount = 1001; // сумма пополнения
-        int bonus_count = 100; // курс бонуса
-        int total_amount; // = count + deposit_amount/bonus_count; // итоговая сумма на счету
+        int depositAmount = 1001; // сумма пополнения
+        int bonusCount = 100; // курс бонуса
+        int totalAmount; // = count + deposit_amount/bonus_count; // итоговая сумма на счету
 
-        if (deposit_amount > 1000) {
-            total_amount = deposit_amount/bonus_count + deposit_amount + count;
+        if (depositAmount > 1000) {
+            totalAmount = depositAmount / bonusCount + depositAmount + count;
 
+        } else {
+            totalAmount = depositAmount + count;
         }
-        else {total_amount = deposit_amount + count;}
 
         System.out.println("Итоговая сумма на счету клиента:");
-        System.out.println((total_amount) + " рублей");
-
+        System.out.println((totalAmount) + " рублей");
 
 
     }
